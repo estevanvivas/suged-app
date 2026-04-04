b44bd22aa10d19a48b96b8fe9d7a7acba0c8dc09
Estevan Vivas <estevanvivas604@gmail.com>
2026-04-04 18:46:50 -0500

    Refactor file structure and update Supabase client initialization

--- backend/src/infrastructure/database/supabase/client.ts
+++ backend/src/infrastructure/database/supabase/client.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
-import {env} from "../config/env";
+import {env} from "../../../config/env";
 
 const supabaseUrl = env.SUPABASE_URL;
 const supabaseServiceKey = env.SUPABASE_SERVICE_ROLE_KEY;
 
-export const supabaseAdmin = createClient(supabaseUrl, supabaseServiceKey);
+export const supabaseClient = createClient(supabaseUrl, supabaseServiceKey);
