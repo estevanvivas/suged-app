0223257bbb415c321235ddff68c5fa726b914867
Estevan Vivas <estevanvivas604@gmail.com>
2026-04-12 16:20:12 -0500

    Refactor TimeSlot class to use value objects for date-time and add validation for start and end times

--- backend/src/core/domain/value-objects/time-slot.ts
+++ backend/src/core/domain/value-objects/time-slot.ts
@@ -1,16 +1,17 @@
-import {DateTime} from 'luxon';
+import {DateTime} from "@/core/domain/value-objects/date-time";
+import {InvalidDateTimeError} from "@/core/domain/errors/invalid-date-time.error";
 
 export class TimeSlot {
     constructor(
         public readonly start: DateTime,
         public readonly end: DateTime
     ) {
+        if (!start.isBefore(end)) {
+            throw new InvalidDateTimeError("El inicio debe ser menor que el fin.");
+        }
     }
 
     overlaps(other: TimeSlot): boolean {
-        return (
-            this.start < other.end &&
-            other.start < this.end
-        );
+        return this.start.isBefore(other.end) && other.start.isBefore(this.end);
     }
 }
